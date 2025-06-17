82c29343c4bb0ba4d0280d819aa0492374d77f5e
murmeowcode <grisha23.01.2004@gmail.com>
2025-06-17 09:50:22 +0300

    update room api with crud_base

--- test_app/api/meeting_room.py
+++ test_app/api/meeting_room.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from test_app.core.db import get_async_session
-from test_app.crud.meeting_room import create_meeting_room, get_meeting_room_by_id,get_room_id_by_name,read_all_rooms_from_db, update_meeting_room,delete_meeting_room
-from test_app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB, MeetingRoomUpdate
+from test_app.crud.meeting_room import meeting_room_crud
+from test_app.models.meeting_room import MeetingRoom
+from test_app.schemas.meeting_room import (MeetingRoomCreate,
+                                           MeetingRoomDB, MeetingRoomUpdate)
 
 router = APIRouter(prefix='/meeting_rooms')
 
@@ -12,36 +14,36 @@ async def create_new_meeting_room(
         meeting_room: MeetingRoomCreate, session: AsyncSession = Depends(get_async_session)
 ):
     await check_name_duplicate(meeting_room.name,session)
-    new_room = await create_meeting_room(meeting_room, session)
+    new_room = await meeting_room_crud.create(meeting_room, session)
     return new_room
 
 @router.get('/',response_model=list[MeetingRoomDB],response_model_exclude_none=True)
 async def get_all_meeting_rooms(session: AsyncSession = Depends(get_async_session)):
-    rooms = await read_all_rooms_from_db(session)
+    rooms = await meeting_room_crud.get_multi(session)
     return rooms
 
 @router.patch('/{meeting_room_id}',response_model=MeetingRoomUpdate,response_model_exclude_none=True)
 async def partially_update_meeting_room(meeting_room_id : int, update_data : MeetingRoomUpdate,session : AsyncSession = Depends(get_async_session)):
-    room = await get_meeting_room_by_id(meeting_room_id,session)
+    room = await meeting_room_crud.get(meeting_room_id,session)
     if room is None:
         raise HTTPException(status_code=404,
                             detail="Переговорка не найдена!")
     if update_data.name is not None:
         await check_name_duplicate(update_data.name,session)
-    room = await update_meeting_room(room,update_data,session)
+    room = await meeting_room_crud.update(room,update_data,session)
     return room
 
 @router.delete('/{meeting_room_id}',response_model=MeetingRoomDB,response_model_exclude_none=True)
 async def remove_meeting_room(meeting_room_id : int, session : AsyncSession = Depends(get_async_session)):
-    room = await get_meeting_room_by_id(meeting_room_id,session)
+    room = await meeting_room_crud.get(meeting_room_id,session)
     if room is None:
         raise HTTPException(status_code=404,
                             detail="Переговорка не найдена!")
-    meeting_room = await delete_meeting_room(room,session)
+    meeting_room = await meeting_room_crud.remove(room,session)
     return meeting_room    
 
 async def check_name_duplicate(room_name : str, session : AsyncSession) -> None:
-    room_id = await get_room_id_by_name(room_name, session)
+    room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
