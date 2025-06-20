361027b01f0b1b621dcf6e52a790e887c0578559
murMeowCode <grisha23.01.2004@gmail.com>
2025-06-20 12:28:02 +0300

    update coroutine get_res_smtm

--- test_app/crud/reservation.py
+++ test_app/crud/reservation.py
@@ -1,6 +1,6 @@
 """CRUD operations for reservetion"""
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 from sqlalchemy import and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from test_app.crud.base import CRUDBase
@@ -13,9 +13,10 @@ class CRUDReservation(CRUDBase):
         CRUDBase (_type_): _description_
     """
 
-    async def get_reservations_at_the_same_time(self, from_reserve : datetime,
+    async def get_reservations_at_the_same_time(self,*, from_reserve : datetime,
                                                 to_reserve : datetime,
                                                 meetingroom_id : int,
+                                                reservation_id : Optional[int] = None,
                                                 session : AsyncSession) -> List[Reservation]:
         """_summary_
 
@@ -28,14 +29,17 @@ class CRUDReservation(CRUDBase):
         Returns:
             List[Reservation]: _description_
         """
-        rooms = await session.execute(
-            select(Reservation).where(
+        select_smt = select(Reservation).where(
                 Reservation.meetingroom_id == meetingroom_id,
                 and_(
                     from_reserve <= Reservation.to_reserve,
                     to_reserve >= Reservation.from_reserve
                 ))
-        )
+        if reservation_id is not None:
+            select_smt = select_smt.where(
+                Reservation.id != reservation_id
+            )
+        rooms = await session.execute(select_smt)
         rooms = rooms.scalars.all()
         return rooms
 
