9a7d3c219708503c74861c0ec1a1d7f2f3743ebc
murMeowCode <grisha23.01.2004@gmail.com>
2025-06-20 12:43:55 +0300

    upd extra forbid for reservation

--- test_app/schemas/reservation.py
+++ test_app/schemas/reservation.py
@@ -1,31 +1,35 @@
-from typing import Optional
-from pydantic import BaseModel,field_validator,model_validator
+"""schemas for reservations"""
 from datetime import datetime
+from pydantic import BaseModel,field_validator,model_validator
+
 
 class ReservationBase(BaseModel):
     from_reserve : datetime
     to_reserve : datetime
-    
+
+    class Config:
+        extra = "forbid"
+
 class ReservationUpdate(ReservationBase):
-    
+
     @field_validator('from_reserve')
-    def check_from_reserve_later_than_now(cls,value):
+    def check_from_reserve_later_than_now(cls,value): #pylint: disable=E0213
         if value <= datetime.now():
             raise ValueError("Время начала бронирования должно быть большего текущего")
         return value
-    
+
     @model_validator(skip_on_failure=True)
-    def check_from_reserve_less_than_to_reserve(cls,values):
+    def check_from_reserve_less_than_to_reserve(cls,values): #pylint: disable=E0213
         if values['from_reserve'] >= values['to_reserve']:
             raise ValueError("Время начала бронирования должно быть меньше времени окончания")
         return values
-    
+
 class ReservationCreate(ReservationUpdate):
     meetingroom_id : int
 
 class ReservationDB(ReservationBase):
     id : int
     meetingroom_id : int
-    
+
     class Config:
         from_attributes = True
