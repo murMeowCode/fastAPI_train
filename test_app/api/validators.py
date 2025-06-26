0fc741aee306ba7fe5cbb900220408d1418a8828
murMeowCode <grisha23.01.2004@gmail.com>
2025-06-26 15:41:38 +0300

    update validators

--- test_app/api/validators.py
+++ test_app/api/validators.py
@@ -1,3 +1,4 @@
+"""module for param validations"""
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,17 +7,38 @@ from test_app.crud.reservation import reservation_crud
 from test_app.models.meeting_room import MeetingRoom
 
 async def check_name_duplicate(room_name : str, session : AsyncSession) -> None:
+    """_summary_
+
+    Args:
+        room_name (str): _description_
+        session (AsyncSession): _description_
+
+    Raises:
+        HTTPException: _description_
+    """
     room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже существует!',
         )
-    
+
 async def check_meeting_room_exists(
         meeting_room_id: int,
         session: AsyncSession,
 ) -> MeetingRoom:
+    """_summary_
+
+    Args:
+        meeting_room_id (int): _description_
+        session (AsyncSession): _description_
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        MeetingRoom: _description_
+    """
     meeting_room = await meeting_room_crud.get(meeting_room_id, session)
     if meeting_room is None:
         raise HTTPException(
@@ -39,6 +61,18 @@ async def check_reservation_interceptions(**kwargs) -> None:
 
 async def check_reservation_before_edit(reservation_id : int,
                                          session : AsyncSession):
+    """_summary_
+
+    Args:
+        reservation_id (int): _description_
+        session (AsyncSession): _description_
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
+    """
     reservation = reservation_crud.get(reservation_id,session)
     if not reservation:
         raise HTTPException(
