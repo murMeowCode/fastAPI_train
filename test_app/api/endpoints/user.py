9e8068c66d786eefb4cd1f080fe7ff8e334a2c60
murMeowCode <grisha23.01.2004@gmail.com>
2025-06-27 08:32:02 +0300

    overdefine user delete

--- test_app/api/endpoints/user.py
+++ test_app/api/endpoints/user.py
@@ -1,5 +1,5 @@
 """routers for users ops"""
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
 
 from test_app.core.user import auth_backend, fastapi_users
 from test_app.schemas.user import UserCreate, UserRead, UserUpdate
@@ -23,3 +23,18 @@ router.include_router(
     prefix='/users',
     tags=['users']
 )
+
+@router.delete(
+    # Путь и тег полностью копируют параметры эндпоинта по умолчанию.
+    '/users/{id}',
+    tags=['users'],
+    # Параметр, который показывает, что метод устарел.
+    deprecated=True
+)
+def delete_user(id: str):  #pylint: disable=W0622
+    """Не используйте удаление, деактивируйте пользователей."""
+    raise HTTPException(
+        # 405 ошибка - метод не разрешен.
+        status_code=405,
+        detail="Удаление пользователей запрещено!"
+    ) 
